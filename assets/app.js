c589813fb2e6c5906c001769d808f8d3d03b8ebb
silenthill99 <florian.graziani@sfr.fr>
2025-11-23 20:15:00 +0100

    Maj du projet

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './stimulus_bootstrap.js';
+import './vendor/bootstrap/dist/css/bootstrap.min.css'
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,3 +9,29 @@ import './stimulus_bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    }
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
--- importmap.php
+++ importmap.php
@@ -25,4 +25,21 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'tailwindcss' => [
+        'version' => '4.1.17',
+    ],
+    'tailwindcss/index.min.css' => [
+        'version' => '4.1.17',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
 ];
